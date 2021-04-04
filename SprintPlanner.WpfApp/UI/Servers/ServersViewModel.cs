f9fb85ff1f682dc9bcc890ebcbf9ac10c58dc149
Remus Octavian Pop <ropop@sdl.com>
2021-04-04 12:01:21 +0300

    WIP adding servers view

--- SprintPlanner.WpfApp/UI/Servers/ServersViewModel.cs
+++ SprintPlanner.WpfApp/UI/Servers/ServersViewModel.cs
@@ -1,8 +1,14 @@
 ﻿using SprintPlanner.FrameworkWPF;
+using System.Collections.ObjectModel;
 
 namespace SprintPlanner.WpfApp.UI.Servers
 {
     public class ServersViewModel : ViewModelBase
     {
+        public ObservableCollection<ServerItemViewModel> Servers
+        {
+            get { return Get(() => Servers); }
+            set { Set(() => Servers, value); }
+        }
     }
 }

38644d66b3428cb4a4b785c17d66d390b3566c06
Remus Octavian Pop <ropop@sdl.com>
2021-04-04 12:01:21 +0300

    WIP adding servers view

--- SprintPlanner.WpfApp/DesignTime/DesignData.cs
+++ SprintPlanner.WpfApp/DesignTime/DesignData.cs
@@ -1,4 +1,5 @@
 ﻿using SprintPlanner.WpfApp.UI.Servers;
+using System.Collections.ObjectModel;
 
 namespace SprintPlanner.WpfApp.DesignTime
 {
@@ -10,6 +11,34 @@ namespace SprintPlanner.WpfApp.DesignTime
             Url = "https://www.mysuperurl.com/link",
             StoryPointsField = "customfield_1234",
             IsActive = true
+        };
+
+        public static ServersViewModel ServersViewModelDesign => new ServersViewModel
+        {
+            Servers = new ObservableCollection<ServerItemViewModel>
+            {
+                new ServerItemViewModel
+                {
+                    ServerName = "My test Server Name",
+                    Url = "https://www.google.com/",
+                    StoryPointsField = "customfield_1234",
+                    IsActive = false
+                },
+                new ServerItemViewModel
+                {
+                    ServerName = "My test Server Name Super Duper Default",
+                    Url = "https://www.mysuperurl.com/link",
+                    StoryPointsField = "customfield_1234",
+                    IsActive = true
+                },
+                new ServerItemViewModel
+                {
+                    ServerName = "Some JIRA",
+                    Url = "https://www.mysuperurl.com/jira/specific/value",
+                    StoryPointsField = "customfield_12341234123412341234",
+                    IsActive = false
+                }
+            }
         };
     }
 }
